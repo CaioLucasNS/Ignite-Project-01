82343731ea500591c9624b6b096de7838a0b39e5
DESKTOP-PKAI836\Caio <caio.lucas.do.silva@accenture.com>
2022-10-26 01:25:31 -0300

    initial commit with styles and initial structure

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Caio Lucas"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, inventore corporis voluptatem architecto iure alias id dignissimos natus dolorem veniam porro, totam adipisci asperiores pariatur consequuntur. Corporis ex ea asperiores?"
+          />
+          <Post
+            author="Carlos Lima"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, inventore corporis voluptatem architecto iure alias id dignissimos natus dolorem veniam porro, totam adipisci asperiores pariatur consequuntur. Corporis ex ea asperiores?"
+          />
+          <Post
+            author="Thiago Franca"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, inventore corporis voluptatem architecto iure alias id dignissimos natus dolorem veniam porro, totam adipisci asperiores pariatur consequuntur. Corporis ex ea asperiores?"
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
