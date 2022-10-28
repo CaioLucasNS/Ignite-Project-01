800ad9a6eefb62333f701d6c60b8687d351b4c14
DESKTOP-PKAI836\Caio <caio.lucas.do.silva@accenture.com>
2022-10-28 02:34:14 -0300

    adicionado component Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/caiolucasns.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Caio Lucas</strong>
+
+              <time
+                title="26 de Outubro às 08:00h"
+                dateTime="2022-10-26 08:00:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
