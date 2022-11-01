19ca849cfe9ac3ed32b570cc59a12aa81718b4ac
DESKTOP-PKAI836\Caio <caio.lucas.do.silva@accenture.com>
2022-11-01 15:33:47 -0300

    adicionado componente Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/caiolucasns.png"
-        />
+        <Avatar src="https://github.com/caiolucasns.png"/>
 
         <strong>Caio Lucas</strong>
         <span>Web Developer</span>
