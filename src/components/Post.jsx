62184aef9d333b581b57149e6ceae0a4303d512e
DESKTOP-PKAI836\Caio <caio.lucas.do.silva@accenture.com>
2022-11-08 23:24:10 -0300

    deletando comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,91 +7,100 @@ import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post({ author, content, publishedAt }) {
-    const [comments, setComments] = useState(["Post muito bacana"]);
-    const [newCommentText, setNewCommentText] = useState("");
+  const [comments, setComments] = useState(["Post muito bacana"]);
+  const [newCommentText, setNewCommentText] = useState("");
 
-    const publishedDateFormatted = format(
-        publishedAt,
-        "d 'de' LLLL 'às' HH:mm'h'",
-        {
-            locale: ptBR,
-        }
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(
+      (comment) => comment !== commentToDelete
     );
 
-    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
-        locale: ptBR,
-        addSuffix: true,
-    });
+    setComments(commentsWithoutDeletedOne);
+  }
 
-    function handleCreateNewComment() {
-        event.preventDefault();
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
 
-        setComments([...comments, newCommentText]);
-        setNewCommentText("");
-    }
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
 
-    function handleNewCommentChange() {
-        setNewCommentText(event.target.value);
-    }
+      <div className={styles.content}>
+        {content.map((line) => {
+          switch (line.type) {
+            case "paragraph":
+              return <p key={line.content}>{line.content}</p>;
+            case "link":
+              return (
+                <p key={line.content}>
+                  <a href="#">{line.content}</a>
+                </p>
+              );
+            default:
+              break;
+          }
+        })}
+      </div>
 
-    return (
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar src={author.avatarUrl} />
-                    <div className={styles.authorInfo}>
-                        <strong>{author.name}</strong>
-                        <span>{author.role}</span>
-                    </div>
-                </div>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
 
-                <time
-                    title={publishedDateFormatted}
-                    dateTime={publishedAt.toISOString()}
-                >
-                    {publishedDateRelativeToNow}
-                </time>
-            </header>
+        <textarea
+          name="comment"
+          placeholder="deixe um comentário"
+          onChange={handleNewCommentChange}
+          value={newCommentText}
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
 
-            <div className={styles.content}>
-                {content.map((line) => {
-                    switch (line.type) {
-                        case "paragraph":
-                            return <p key={line.content}>{line.content}</p>;
-                        case "link":
-                            return (
-                                <p key={line.content}>
-                                    <a href="#">{line.content}</a>
-                                </p>
-                            );
-                        default:
-                            break;
-                    }
-                })}
-            </div>
-
-            <form
-                onSubmit={handleCreateNewComment}
-                className={styles.commentForm}
-            >
-                <strong>Deixe seu feedback</strong>
-
-                <textarea
-                    name="comment"
-                    placeholder="deixe um comentário"
-                    onChange={handleNewCommentChange}
-                    value={newCommentText}
-                />
-                <footer>
-                    <button type="submit">Publicar</button>
-                </footer>
-            </form>
-
-            <div className={styles.commentList}>
-                {comments.map((comment) => (
-                    <Comment key={comment} content={comment} />
-                ))}
-            </div>
-        </article>
-    );
+      <div className={styles.commentList}>
+        {comments.map((comment) => (
+          <Comment
+            key={comment}
+            content={comment}
+            onDeleteComment={deleteComment}
+          />
+        ))}
+      </div>
+    </article>
+  );
 }
