edcd2e44984caccb074b26565cbe84ce7e5347ab
Caio Lucas <caio.lucas.do.silva@accenture.com>
2022-11-08 23:03:23 -0300

    adicionado mapeamento de comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,39 +2,43 @@ import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
-export function Comment() {
-  return (
-    <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/caiolucasns.png" alt="" />
+export function Comment({ content }) {
+    return (
+        <div className={styles.comment}>
+            <Avatar
+                hasBorder={false}
+                src="https://github.com/caiolucasns.png"
+                alt=""
+            />
 
-      <div className={styles.commentBox}>
-        <div className={styles.commentContent}>
-          <header>
-            <div className={styles.authorAndTime}>
-              <strong>Caio Lucas</strong>
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Caio Lucas</strong>
 
-              <time
-                title="26 de Outubro às 08:00h"
-                dateTime="2022-10-26 08:00:00"
-              >
-                Cerca de 1h atrás
-              </time>
+                            <time
+                                title="26 de Outubro às 08:00h"
+                                dateTime="2022-10-26 08:00:00"
+                            >
+                                Cerca de 1h atrás
+                            </time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
             </div>
-
-            <button title='Deletar comentário'>
-              <Trash size={24} />
-            </button>
-          </header>
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
         </div>
-
-        <footer>
-          <button>
-            <ThumbsUp />
-            Aplaudir <span>20</span>
-          </button>
-        </footer>
-      </div>
-    </div>
-  );
+    );
 }
